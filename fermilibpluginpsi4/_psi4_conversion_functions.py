38768ff1a650064434358a50e8f720cc7bd9a665
Jarrod <jarrod.mcc@gmail.com>
2017-07-17 20:13:41 -0700

    Update CC amplitude loading (#8)

--- fermilibpluginpsi4/_psi4_conversion_functions.py
+++ fermilibpluginpsi4/_psi4_conversion_functions.py
@@ -253,8 +253,4 @@ def parse_psi4_ccsd_amplitudes(number_orbitals,
                               alpha_unoccupied(b),
                               alpha_occupied(j)] = value / 2.
 
-    # Package into InteractionOperator.
-    molecule = InteractionOperator(0.0,
-                                   single_amplitudes,
-                                   double_amplitudes)
-    return molecule
+    return single_amplitudes, double_amplitudes
